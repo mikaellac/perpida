0344d2a8d701be4a69a49689364a9c4caff616ef
Mikaella Campos <camposbcm@gmail.com>
2024-04-09 21:45:02 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', loadChildren: () => import('./user/user-routing.module').then(m => m.UserRoutingModule) },
+  { path: 'products', loadChildren: () => import('./product/product-routing.module').then(m => m.ProductRoutingModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
